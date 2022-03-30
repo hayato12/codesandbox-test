a7d52e39a1f2aeff65d15336fba75e1b0ce85aac
hayato12 <50575834+hayato12@users.noreply.github.com>
2022-03-30 15:01:02 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -153,11 +153,29 @@ const nameArr = ["田中", "山田", "ソタ"];
 // });
 // console.log(newNumArr);
 
-const newNumArr = nameArr.map((name) => {
-  if (name === "ソタ") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNumArr);
+// const newNumArr = nameArr.map((name) => {
+//   if (name === "ソタ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNumArr);
+
+// 三項演算子
+//ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 < 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = "1300";
+// console.log(num.toLocaleString());
+
+// const formatedNum =
+//   typeof num === "number" ? num.toLocaleString : "数値を入力してください。";
+// console.log(formatedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? "100超えています" : "許容範囲ないです。";
+// };
+
+// console.log(checkSum(50, 6));
