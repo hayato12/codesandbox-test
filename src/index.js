5db59e6058f767f29783b074c64a67a6b4c77733
hayato12 <50575834+hayato12@users.noreply.github.com>
2022-03-30 15:07:43 +0900

    論理演算子の本当の意味を知ろう

--- src/index.js
+++ src/index.js
@@ -179,3 +179,24 @@ const nameArr = ["田中", "山田", "ソタ"];
 // };
 
 // console.log(checkSum(50, 6));
+
+//論理演算子の本当の意味を知ろう && ||
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か２はtrueになります");
+// }
+// if (flag1 && flag2) {
+//   console.log("1と２はtrueになります");
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || "キンガクミセッテイデス;";
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
