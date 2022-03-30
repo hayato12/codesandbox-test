82eaead7e7e5306ba6a3eadb0eed9a6c4ca498b3
hayato12 <50575834+hayato12@users.noreply.github.com>
2022-03-30 14:55:20 +0900

    mapやfilterを使った配列の処理

--- src/index.js
+++ src/index.js
@@ -119,13 +119,45 @@
 // console.log(arr3);
 
 // 配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
 
-const arr6 = [...arr4];
-arr6[0] = 100;
-console.log(arr6);
-console.log(arr4);
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
 
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
+
+//mapやfilterを使った配列の処理
+const nameArr = ["田中", "山田", "ソタ"];
+//従来
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です。`);
+// }
+
+// mapを使ったら
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name, index) =>
+//   console.log(`${index + 1}番目は${nameArr[index]}です。`)
+// );
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+const newNumArr = nameArr.map((name) => {
+  if (name === "ソタ") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(newNumArr);
